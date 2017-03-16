141cbf756189c7d99cb2b479a897e8c6995bce92
avenirs05 <avenirs05@gmail.com>
2017-03-16 17:45:29 +0300

    16.03.17 all done besides contacts page

--- page-freski.php
+++ page-freski.php
@@ -1,11 +1,25 @@
 <?php get_header(); ?>
 
+<?php global $post; ?>
+<?php $post_slug = $post->post_name; ?>	
+
 <div class="container-fluid" id="pages-main">
 	<div class="row">
-        <div class="col-md-8 col-md-offset-2">		
-			
-		Фрески
-			
+       	<div class="col-md-10 col-md-offset-1 content-freski">
+
+<?php $factories = new WP_Query(array ('category_name' => "$post_slug")); ?>
+	<?php if ($factories->have_posts()) : 
+	  		while ($factories->have_posts()) : 
+			  	    $factories->the_post(); ?>	
+					 <?php $post_slug = $post->post_name; ?>		
+						<div>
+							<div>
+								<?php the_post_thumbnail(); ?>
+							</div>
+							<?php the_content(); ?>				
+						</div>										
+				<?php endwhile; ?>
+			<?php endif; ?>		
 		</div>
 	</div>
 </div>
